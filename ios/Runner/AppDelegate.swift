a8c574522ea7d9f11e3264dccd2d2c52c2f48356
Cinthia Pineroli Galvao <cinthia.galvao@eldorado.org.br>
2018-07-10 13:40:27 -0300

    Chamada nativa iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,26 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController;
+    let channel = FlutterMethodChannel.init(name: "tdc_demo_channel",
+                                                   binaryMessenger: controller);
+    channel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        if (call.method == "getNativeText") {
+            var resultString = "Método iOS, parâmetro:"
+            resultString += " \(call.arguments ?? "")"
+            
+            channel.invokeMethod("getFlutterText", arguments: "iOSParam", result: { result in
+                resultString += " \(result ?? "")"
+                result(resultString)
+            })
+            
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    });
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
