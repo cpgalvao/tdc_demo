f36ea037d90f121288156052a09c52b8645929b4
Cinthia Pineroli Galvão <cpgalvao@gmail.com>
2018-07-14 19:33:01 -0300

    Ajustes layout

--- lib/detail.dart
+++ lib/detail.dart
@@ -19,40 +19,42 @@ class _DetailPage extends State<DetailPage> {
         appBar: AppBar(
           title: Text(S.of(context).flutter_tdc_demo),
         ),
-        body: Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Hero(
-                  tag: widget.data.name,
-                  child: Material(
-                    color: Colors.transparent,
-                    child: Text(
-                      widget.data.emoji,
-                      style: TextStyle(fontSize: 80.0),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: widget.data.name,
+                child: Material(
+                  color: Colors.transparent,
                   child: Text(
-                    widget.data.name,
-                    style: TextStyle(fontSize: 40.0),
+                    widget.data.emoji,
+                    style: TextStyle(fontSize: 80.0),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 10.0),
-                  child: Text(
-                    '$_fruitNumber/${widget.data.availableNumber.toString()}',
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  widget.data.name,
+                  style: TextStyle(fontSize: 40.0),
                 ),
-                Text(
-                  '${S.of(context).currency}\$ ${widget.data.price.toString()}',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '$_fruitNumber/${widget.data.availableNumber.toString()}',
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '${S
+                      .of(context)
+                      .currency}\$ ${widget.data.price.toString()}',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                 ),
-              ],
-            )),
+              ),
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: _buyItem,
           tooltip: S.of(context).buy,
